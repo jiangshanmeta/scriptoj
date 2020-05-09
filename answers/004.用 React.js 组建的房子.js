bf5f1c194f8f3437f07bb1cb9badbc1e482fe0e7
jiangshanmeta <540118044@qq.com>
2020-05-09 21:50:10 +0800

    4

---
+++ answers/004.用 React.js 组建的房子.js
@@ -0,0 +1,42 @@
+// Component 已经可以直接使用
+
+class House extends Component {
+    render(){
+      return (
+        <div className="house">
+          <Room/>
+          <Bathroom/>
+        </div>
+      )
+    }
+  
+  }
+  
+  class Room extends Component {
+    render(){
+      return (<div className="room">
+        <Man/>
+        <Dog/>
+        <Dog/>
+      </div>)
+    }
+  }
+  
+  class Bathroom extends Component {
+    render(){
+      return (<div className="bathroom"></div>)
+    }
+  }
+  
+  class Man extends Component {
+    render(){
+      return (<div className="man"></div>)
+    }
+  }
+  
+  class Dog extends Component {
+    render(){
+      return (<div className="dog"></div>)
+    }
+  }
+  
